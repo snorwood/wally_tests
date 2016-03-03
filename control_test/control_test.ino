bf545fd52bbff47eb5a837f8bd52d39d782af99e
unknown <steven@snorwood.com>
2016-03-03 11:16:13 -0500

    Accelerometer data read trial complete

--- control_test/control_test.ino
+++ control_test/control_test.ino
@@ -1,15 +1,31 @@
-const int ledPin =  13;
+#include "wally.h"
+
+Wally* wally;
+unsigned long start_time, loop_time;
+const unsigned long TRIAL_TIME = 3e6;
+XYZ acc;
 
 void setup() {
-  pinMode(40, INPUT);
-  pinMode(36, OUTPUT);
-  digitalWrite(36, HIGH);
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  wally = new Wally();
+  wally->waitButton();
+  start_time = micros();
 }
 
 void loop() {
-  digitalWrite(ledPin, LOW);
-  while(!digitalRead(40));
-  digitalWrite(13, HIGH);
-  while(digitalRead(40));
+  /* Drive Motors Back and Forth */
+  loop_time = micros();
+  if (loop_time - start_time < TRIAL_TIME) {
+    wally->setMotors(50, 50);
+    acc = wally->readAccelerometer();
+    Serial.print(loop_time);
+    Serial.print(",");
+    Serial.print(acc.x);
+    Serial.print(",");
+    Serial.print(acc.y);
+    Serial.print(",");
+    Serial.println(acc.z);
+  } else {
+    wally.setMotors(0, 0);
+  }
 }
