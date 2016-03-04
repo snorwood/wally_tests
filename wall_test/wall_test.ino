b72d5c5005be8bfaac44b6fd1be82cc56ef31107
Steven Norwood <steven@snorwood.com>
2016-03-04 18:10:53 -0500

    Added wall test

---
+++ wall_test/wall_test.ino
@@ -0,0 +1,32 @@
+/*
+  wall_test.ino - Tests the profile for climbing up the wall.
+  Created by Team Wall-R-Us, March 4, 2016.
+*/
+
+#include "wally.h"
+
+Wally* wally;
+XYZ acc;
+
+void setup() {
+  wally = new Wally();
+  pinMode(13, OUTPUT);
+  wally->waitButton();
+}
+
+void loop() {
+  /* Drive Motors Back and Forth */
+  loop_time = micros();
+  if (wally->readIR()) {
+    wally->setMotors(10, 100);
+      acc = wally->readAccelerometer();
+  } else {
+    delay(150);
+    wally->setMotorsFR(100, -100);
+    delay(75);
+    wally->setMotorsFR(100, 30);
+    delay(2000);
+    wally->setMotorsFR(0, 0);
+    wally->waitButton();
+  }
+}
