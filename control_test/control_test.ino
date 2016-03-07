321e2b665188d81930ecaed03cb6b00527d415b6
unknown <steven@snorwood.com>
2016-03-07 08:47:09 -0500

    Control loop test ready for tuning

--- control_test/control_test.ino
+++ control_test/control_test.ino
@@ -2,35 +2,32 @@
   control_test.ino - Test environment for control logic.
   Created by Team Wall-R-Us, March 4, 2016.
 */
-
 #include "wally.h"
+#include "wally_control.h"
 
 Wally* wally;
+WallyControl* wallyControl;
+
 unsigned long start_time, loop_time;
-const unsigned long TRIAL_TIME = 2e6;
-XYZ acc;
+const unsigned long TRIAL_TIME = 5e6;
 
 void setup() {
   Serial.begin(115200);
   wally = new Wally();
+  wallyControl = new WallyControl(wally);
   wally->waitButton();
+  delay(1000);
+  wallyControl->begin();
   start_time = micros();
 }
 
 void loop() {
   /* Drive Motors Back and Forth */
   loop_time = micros();
-  if (loop_time - start_time < TRIAL_TIME) {
-    wally->setMotors(45, 45);
-    acc = wally->readAccelerometer();
-    Serial.print(loop_time);
-    Serial.print(",");
-    Serial.print(acc.x);
-    Serial.print(",");
-    Serial.print(acc.y);
-    Serial.print(",");
-    Serial.println(acc.z);
-  } else {
-    wally->setMotors(0, 0);
+  wallyControl->verticalControl();
+  if ((loop_time - start_time > TRIAL_TIME) || wally->readButton()){
+    wally->stop();
+    delay(1000); 
+    setup();
   }
 }
