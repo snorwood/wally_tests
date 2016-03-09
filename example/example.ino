1cee8bafc66095b15c64b1215463c53856b4f109
Steven Norwood <steven@snorwood.com>
2016-03-08 21:37:12 -0500

    Updated example to work with library

--- example/example.ino
+++ example/example.ino
@@ -5,26 +5,27 @@
 
 #include <wally.h>
 
-Wally wally; // Initialize wally robot
+Wally* wally; // Initialize wally robot
 int firstLoop = 1;
 
 void setup() {
 //  /* Wait for Button */
+  wally = new Wally();
   Serial.begin(115200); // Initialize serial 
 }
 
 void loop() {
   Serial.println("Wally Example");  // Output serial string
-  wally.waitButton();
+  wally->waitButton();
   
   /* Read Ultrasonics */
   Serial.print("Front Sensor: ");
-  Serial.println(wally.readUltrasonic(0));
+  Serial.println(wally->readUltrasonic(0));
   Serial.print("Rear Sensor: ");
-  Serial.println(wally.readUltrasonic(1));
+  Serial.println(wally->readUltrasonic(1));
 
   /* Read Accelerometer */
-  XYZ acc = wally.readAccelerometer();
+  XYZ acc = wally->readAccelerometer();
   Serial.print("Acceleration: x=");
   Serial.print(acc.x);
   Serial.print(" y=");
@@ -33,9 +34,9 @@ void loop() {
   Serial.println(acc.z);  
 
   /* Drive Motors Back and Forth */
-  wally.setMotors(50, 50);
-  delay(5000);
-  wally.setMotors(-50, -50);
-  delay(5000);
-  wally.setMotors(0, 0);
+//  wally->setMotors(50, 50);
+//  delay(5000);
+//  wally->setMotors(-50, -50);
+//  delay(5000);
+//  wally->setMotors(0, 0);
 }
