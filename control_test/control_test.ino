943091531a07e5ce17278c4cd5ae841bae9f0eba
Steven Norwood <steven@snorwood.com>
2016-03-08 21:39:17 -0500

    Updated control test to work with newest library rev

--- control_test/control_test.ino
+++ control_test/control_test.ino
@@ -9,25 +9,47 @@ Wally* wally;
 WallyControl* wallyControl;
 
 unsigned long start_time, loop_time;
-const unsigned long TRIAL_TIME = 5e6;
+const unsigned long TRIAL_TIME = 8e6;
+int state;
 
 void setup() {
+  pinMode(13, OUTPUT);
   Serial.begin(115200);
   wally = new Wally();
   wallyControl = new WallyControl(wally);
-  wally->waitButton();
-  delay(1000);
-  wallyControl->begin();
-  start_time = micros();
+  state = 0;
 }
 
 void loop() {
   /* Drive Motors Back and Forth */
+  if (state == 0){
+    digitalWrite(13, HIGH);
+    delay(500);
+    wally->waitButton();
+    delay(500);
+    wallyControl->begin();
+    start_time = micros();
+    state = 1;
+  }
+  
   loop_time = micros();
-  wallyControl->verticalControl();
-  if ((loop_time - start_time > TRIAL_TIME) || wally->readButton()){
-    wally->stop();
-    delay(1000); 
-    setup();
+  if (state == 1)  {
+    wallyControl->horizontalControl(loop_time, 0);
+
+    if (wally->getOrientation(wally->readAccelerometer()) == UP){
+      state = 0;
+    }
+  }
+  
+  if (state == 2) {
+    digitalWrite(13, LOW);
+    wallyControl->verticalControl(loop_time, 75, 0);
+    delay(10000);
+  } else {
+    digitalWrite(13, HIGH);
+  }
+  
+  if (wally->readButton() || (loop_time - start_time > TRIAL_TIME)){
+    state = 0;
   }
 }
