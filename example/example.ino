1d1176848a3e8465a71e66a1eee364acf3b230b9
Steven Norwood <steven@snorwood.com>
2016-03-16 15:20:07 -0400

    merged cary's code

--- example/example.ino
+++ example/example.ino
@@ -1,40 +1,55 @@
-/*
+ /*
   example.ino - Demo code for controlling all aspects of the robot.
   Created by Team Wall-R-Us, March 4, 2016.
 */
 
-#include <wally.h>
-
+#include "wally.h"
+#include "wally_control.h"
 Wally* wally; // Initialize wally robot
-int firstLoop = 1;
+WallyControl* wallyControl;;
+
+int loopCount = 0;
 
 void setup() {
-//  /* Wait for Button */
+  Serial.begin(115200); // Initialize serial 
   wally = new Wally();
   wally->calibrateAccelerometer();
-  Serial.begin(115200); // Initialize serial 
+  wallyControl = new WallyControl(wally);
+  wallyControl->begin();  
 }
 
 void loop() {
-  Serial.println("Wally Example");  // Output serial string
-  wally->waitButton();
+  loopCount += 1;
+//  Serial.println("Wally Example");  // Output serial string
+  delay(1000);
+  /* Wait for button press */
+//  wally->waitButton();
+//  Serial.println("Button Pressed");
   
+  for (int i = 0; i < 10; i++) {
+    wallyControl->update();
+  }
+
   /* Read Ultrasonics */
-  Serial.print("Front Sensor: ");
-  Serial.println(wally->readUltrasonic(0));
-  Serial.print("Rear Sensor: ");
-  Serial.println(wally->readUltrasonic(1));
+//  Serial.print("Front Sensor: ");
+//  Serial.println(wallyControl->readUltrasonic(0));
+//  Serial.print("Right Sensor: ");
+//  Serial.println(wallyControl->readUltrasonic(1));
+//  Serial.print("Left Sensor: ");
+//  Serial.println(wallyControl->readUltrasonic(2));
 
   /* Read Accelerometer */
-  XYZ acc = wally->readAccelerometer();
-  Serial.print("Acceleration: x=");
-  Serial.print(acc.x);
-  Serial.print(" y=");
-  Serial.print(acc.y);
-  Serial.print(" z=");
-  Serial.print(acc.z);
-  Serial.print(" orientation=");
+  XYZ acc = wallyControl->readAccelerometer();
+  Serial.print("Acceleration:\nx=");
+  Serial.println(acc.x);
+  Serial.print("y=");
+  Serial.println(acc.y);
+  Serial.print("z=");
+  Serial.println(acc.z);
+  Serial.print("orientation=");
   Serial.println(wally->getOrientation(acc));  
+  Serial.print("theta=");
+  Serial.println(wally->getTheta    (acc));  
 
   /* Drive Motors Back and Forth */
 //  wally->setMotors(50, 50);
