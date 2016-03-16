9c54e728ffb314b32eb0c04a53b38250593e6b56
UberOmelette <cary.m.xue@gmail.com>
2016-03-15 23:59:36 -0400

    Added files via upload

--- Split_Idea/Split_Idea.ino
+++ Split_Idea/Split_Idea.ino
@@ -8,32 +8,50 @@ WallyControl* wally_c;
 double front_reading = 201;
 double left_reading = 201;
 double right_reading = 201;
-int left_speed = 20;
-int right_speed = 20;
-int delay_speed = 20;
+double left_speed = 25;
+double right_speed = 25;
+int delay_speed = 30;
 int turn_speed = 30;
 int forwards_speed = 30;
-int right_turn_delay = 350;
-int left_turn_delay = 350;
+double max_speed = 40;
+double min_speed = 10;
+int right_turn_delay = 400;
+int left_turn_delay = 400;
+int base_speed = 40;
+double Kd = 0.7;
+double diff_left;
+double diff_right;
 
 void setup() {
   /* Wait for Button */
-  wally->waitButton();
   pinMode(13, OUTPUT);
   Serial.begin(115200);
   wally = new Wally();
-  wally_c = new WallyControl(wally, 1, 1, 1, 5);
+  wally->waitButton();
+  wally_c = new WallyControl(wally, 1, 1, 1, 3);
   wally_c->begin();
   wally_c->update();
   wally_c->update();
   wally_c->update();
   wally_c->update();
   wally_c->update();
+  wally_c->update();
+  wally_c->update();
+  wally_c->update();
+  wally_c->update();
+  wally_c->update();
+  wally_c->update();
+  left_reading = wally_c->readUltrasonic(2);
+  right_reading = wally_c->readUltrasonic(1);
+  
 }
 
 double differentialLeft(double reading) {
   // negative is away
   // positive is towards
+//  if(reading == 201.00) {
+//    return left_reading;
+//  }
   double value = left_reading - reading;
   left_reading = reading;
   return value;
@@ -42,6 +60,9 @@ double differentialLeft(double reading) {
 double differentialRight(double reading) {
   // negative is away
   // positive is towards
+//  if(reading == 201.00) {
+//    return right_reading;
+//  }
   double value = right_reading - reading;
   right_reading = reading;
   return value;
@@ -57,12 +78,13 @@ double differentialFront(double reading) {
 
 int delayTime(double reading, int speed) {
   int value = (int)reading;
-  return value / speed * 100;
+  return (int)(value / speed * 25);
 }
 
 int notCloseToBase() {
   wally_c->update();
-  if(wally_c->readUltrasonic(0) < 15) {
+//  Serial.print("1");
+  if(wally_c->readUltrasonic(0) < 15.00) {
     return 0;
   }
   return 1;
@@ -71,8 +93,10 @@ int notCloseToBase() {
 void loop() {
   wally->setMotors(left_speed,right_speed);
   while(notCloseToBase()) {
+    diff_left = differentialLeft(wally_c->readUltrasonic(2));
+    diff_right = differentialRight(wally_c->readUltrasonic(1));
 //    wally_c->update();
-    if(differentialLeft(wally_c->readUltrasonic(2)) > 30) {
+    if(diff_left > 40) {
       wally->setMotors(delay_speed,delay_speed);
       delay(delayTime(wally_c->readUltrasonic(2),delay_speed));
       wally->setMotors(0,0);
@@ -81,15 +105,21 @@ void loop() {
       delay(left_turn_delay);
       wally->setMotors(0,0);
       delay(100);
-
       wally->setMotors(forwards_speed,forwards_speed);
       while(notCloseToBase()) {
 //        wally_c->update(); 
       }
+      wally->setMotors(forwards_speed,forwards_speed);
+      while(wally->getOrientation(wally_c->readAccelerometer()) == FLAT) {
+        wally_c->update();
+      }
+      while(wally->getOrientation(wally_c->readAccelerometer()) != FLAT) {
+        wally_c->update();
+      }
       wally->setMotors(0,0);
       wally->waitButton();
     }
-    if(differentialRight(wally_c->readUltrasonic(1)) > 30) {
+    if(diff_right > 40) {
       wally->setMotors(delay_speed,delay_speed);
       delay(delayTime(wally_c->readUltrasonic(1),delay_speed));
       wally->setMotors(0,0);
@@ -98,26 +128,83 @@ void loop() {
       delay(right_turn_delay);
       wally->setMotors(0,0);
       delay(100);
-
       wally->setMotors(forwards_speed,forwards_speed);
       while(notCloseToBase()) {
 //        wally_c->update(); 
       }
+      wally->setMotors(forwards_speed,forwards_speed);
+      while(wally->getOrientation(wally_c->readAccelerometer()) == FLAT) {
+        wally_c->update();
+      }
+      while(wally->getOrientation(wally_c->readAccelerometer()) != FLAT) {
+        wally_c->update();
+      }
       wally->setMotors(0,0);
       wally->waitButton();
     }
-    if(differentialLeft(wally_c->readUltrasonic(2)) < 0) {
-      if(left_speed > 10) {
-        left_speed = left_speed - 1;
-        right_speed = right_speed + 1;
-      }
+//    if(differentialRight(wally_c->readUltrasonic(1)) > 40) {
+//      wally->setMotors(delay_speed,delay_speed);
+//      delay(delayTime(wally_c->readUltrasonic(1),delay_speed));
+//      wally->setMotors(0,0);
+//      delay(100);
+//      wally->setMotors(turn_speed,-turn_speed);
+//      delay(right_turn_delay);
+//      wally->setMotors(0,0);
+//      delay(100);
+//
+//      wally->setMotors(forwards_speed,forwards_speed);
+//      while(notCloseToBase()) {
+////        wally_c->update(); 
+//      }
+//      wally->setMotors(0,0);
+//      wally->waitButton();
+//    }
+//    if(diff < 0) {
+//      if(left_speed > 10) {
+//        left_speed = left_speed - diff * Kd;
+//        right_speed = right_speed - diff * Kd;
+//      }
+//      Serial.println("Correcting to Left");
+//      Serial.println(diff);
+//      Serial.print(left_speed);
+//      Serial.print(" <-> ");
+//      Serial.println(right_speed);
+//    }
+//    if(diff > 0) {
+//      if(right_speed > 10) {
+//        left_speed = left_speed + diff * Kd;
+//        right_speed = right_speed - diff * Kd;
+//      }
+//      Serial.println("Correcting to Right");
+//      Serial.println(diff);
+//      Serial.print(left_speed);
+//      Serial.print(" <-> ");
+//      Serial.println(right_speed);
+//    }
+    
+    left_speed = left_speed + diff_left * Kd;
+    right_speed = right_speed - diff_left * Kd;
+    if(left_speed > max_speed) {
+      left_speed = max_speed;
     }
-    if(differentialLeft(wally_c->readUltrasonic(2)) > 0) {
-      if(right_speed > 10) {
-        left_speed = left_speed + 1;
-        right_speed = right_speed - 1;
-      }
+    if(left_speed < min_speed) {
+      left_speed = min_speed;
+    }
+    if(right_speed > max_speed) {
+      right_speed = max_speed;
+    }
+    if(right_speed < min_speed) {
+      right_speed = min_speed;
     }
     wally->setMotors(left_speed,right_speed);
   }
+  wally->setMotors(forwards_speed,forwards_speed);
+  while(wally->getOrientation(wally_c->readAccelerometer()) == FLAT) {
+    wally_c->update();
+  }
+  while(wally->getOrientation(wally_c->readAccelerometer()) != FLAT) {
+    wally_c->update();
+  }
+  wally->setMotors(0,0);
+  wally->waitButton();
 }
